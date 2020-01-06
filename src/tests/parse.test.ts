1daa2855ea82e8c1861bc1344cac7c77cb9ed8ea
mishotek <mtekt16@freeuni.edu.ge>
2020-01-06 17:07:18 +0400

    # Issues with ordering operators

--- src/tests/parse.test.ts
+++ src/tests/parse.test.ts
@@ -473,122 +473,122 @@ test('Should parse stacked binary expression with multiple parenthesis inside of
     expect(parse(tokens)).toEqual(ast);
 });
 
-// test('Should parse multi line expression', () => {
-//     const tokens: Token[] = tokenize(`1 + 2; (2) + 5 / 12 * (1 + (2 + 5));`);
-//
-//     const ast = {
-//         "type": "Program",
-//         "start": 0,
-//         "end": 36,
-//         "body": [
-//             {
-//                 "type": "ExpressionStatement",
-//                 "start": 0,
-//                 "end": 6,
-//                 "expression": {
-//                     "type": "BinaryExpression",
-//                     "start": 0,
-//                     "end": 5,
-//                     "left": {
-//                         "type": "Literal",
-//                         "start": 0,
-//                         "end": 1,
-//                         "value": 1,
-//                         "raw": "1"
-//                     },
-//                     "operator": "+",
-//                     "right": {
-//                         "type": "Literal",
-//                         "start": 4,
-//                         "end": 5,
-//                         "value": 2,
-//                         "raw": "2"
-//                     }
-//                 }
-//             },
-//             {
-//                 "type": "ExpressionStatement",
-//                 "start": 7,
-//                 "end": 36,
-//                 "expression": {
-//                     "type": "BinaryExpression",
-//                     "start": 7,
-//                     "end": 35,
-//                     "left": {
-//                         "type": "Literal",
-//                         "start": 8,
-//                         "end": 9,
-//                         "value": 2,
-//                         "raw": "2"
-//                     },
-//                     "operator": "+",
-//                     "right": {
-//                         "type": "BinaryExpression",
-//                         "start": 13,
-//                         "end": 35,
-//                         "left": {
-//                             "type": "BinaryExpression",
-//                             "start": 13,
-//                             "end": 19,
-//                             "left": {
-//                                 "type": "Literal",
-//                                 "start": 13,
-//                                 "end": 14,
-//                                 "value": 5,
-//                                 "raw": "5"
-//                             },
-//                             "operator": "/",
-//                             "right": {
-//                                 "type": "Literal",
-//                                 "start": 17,
-//                                 "end": 19,
-//                                 "value": 12,
-//                                 "raw": "12"
-//                             }
-//                         },
-//                         "operator": "*",
-//                         "right": {
-//                             "type": "BinaryExpression",
-//                             "start": 23,
-//                             "end": 34,
-//                             "left": {
-//                                 "type": "Literal",
-//                                 "start": 23,
-//                                 "end": 24,
-//                                 "value": 1,
-//                                 "raw": "1"
-//                             },
-//                             "operator": "+",
-//                             "right": {
-//                                 "type": "BinaryExpression",
-//                                 "start": 28,
-//                                 "end": 33,
-//                                 "left": {
-//                                     "type": "Literal",
-//                                     "start": 28,
-//                                     "end": 29,
-//                                     "value": 2,
-//                                     "raw": "2"
-//                                 },
-//                                 "operator": "+",
-//                                 "right": {
-//                                     "type": "Literal",
-//                                     "start": 32,
-//                                     "end": 33,
-//                                     "value": 5,
-//                                     "raw": "5"
-//                                 }
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         ],
-//         "sourceType": "module"
-//     };
-//
-//     expect(parse(tokens)).toEqual(ast);
-// });
+test('Should parse multi line expression', () => {
+    const tokens: Token[] = tokenize(`1 + 2; (2) + 5 + 12 - (1 + (2 + 5));`);
+
+    const ast = {
+        "type": "Program",
+        "start": 0,
+        "end": 36,
+        "body": [
+            {
+                "type": "ExpressionStatement",
+                "start": 0,
+                "end": 6,
+                "expression": {
+                    "type": "BinaryExpression",
+                    "start": 0,
+                    "end": 5,
+                    "left": {
+                        "type": "Literal",
+                        "start": 0,
+                        "end": 1,
+                        "value": 1,
+                        "raw": "1"
+                    },
+                    "operator": "+",
+                    "right": {
+                        "type": "Literal",
+                        "start": 4,
+                        "end": 5,
+                        "value": 2,
+                        "raw": "2"
+                    }
+                }
+            },
+            {
+                "type": "ExpressionStatement",
+                "start": 7,
+                "end": 36,
+                "expression": {
+                    "type": "BinaryExpression",
+                    "start": 7,
+                    "end": 35,
+                    "left": {
+                        "type": "BinaryExpression",
+                        "start": 7,
+                        "end": 19,
+                        "left": {
+                            "type": "BinaryExpression",
+                            "start": 7,
+                            "end": 14,
+                            "left": {
+                                "type": "Literal",
+                                "start": 8,
+                                "end": 9,
+                                "value": 2,
+                                "raw": "2"
+                            },
+                            "operator": "+",
+                            "right": {
+                                "type": "Literal",
+                                "start": 13,
+                                "end": 14,
+                                "value": 5,
+                                "raw": "5"
+                            }
+                        },
+                        "operator": "+",
+                        "right": {
+                            "type": "Literal",
+                            "start": 17,
+                            "end": 19,
+                            "value": 12,
+                            "raw": "12"
+                        }
+                    },
+                    "operator": "-",
+                    "right": {
+                        "type": "BinaryExpression",
+                        "start": 23,
+                        "end": 34,
+                        "left": {
+                            "type": "Literal",
+                            "start": 23,
+                            "end": 24,
+                            "value": 1,
+                            "raw": "1"
+                        },
+                        "operator": "+",
+                        "right": {
+                            "type": "BinaryExpression",
+                            "start": 28,
+                            "end": 33,
+                            "left": {
+                                "type": "Literal",
+                                "start": 28,
+                                "end": 29,
+                                "value": 2,
+                                "raw": "2"
+                            },
+                            "operator": "+",
+                            "right": {
+                                "type": "Literal",
+                                "start": 32,
+                                "end": 33,
+                                "value": 5,
+                                "raw": "5"
+                            }
+                        }
+                    }
+                }
+            }
+        ],
+        "sourceType": "module"
+    };
+
+    expect(parse(tokens)).toEqual(ast);
+});
 
 test('Should parse simple variable declaration', () => {
     const tokens: Token[] = tokenize('var a = 2;');
@@ -677,6 +677,72 @@ test('Should parse simple constant variable declaration with binary operation', 
                     }
                 ],
                 "kind": "const"
+            }
+        ],
+        "sourceType": "module"
+    };
+
+    expect(parse(tokens)).toEqual(ast);
+});
+
+test('Should prioritize * and / operators', () => {
+    const tokens: Token[] = tokenize('1 + 2 * 3 + 4;');
+
+    const ast = {
+        "type": "Program",
+        "start": 0,
+        "end": 14,
+        "body": [
+            {
+                "type": "ExpressionStatement",
+                "start": 0,
+                "end": 14,
+                "expression": {
+                    "type": "BinaryExpression",
+                    "start": 0,
+                    "end": 13,
+                    "left": {
+                        "type": "BinaryExpression",
+                        "start": 0,
+                        "end": 9,
+                        "left": {
+                            "type": "Literal",
+                            "start": 0,
+                            "end": 1,
+                            "value": 1,
+                            "raw": "1"
+                        },
+                        "operator": "+",
+                        "right": {
+                            "type": "BinaryExpression",
+                            "start": 4,
+                            "end": 9,
+                            "left": {
+                                "type": "Literal",
+                                "start": 4,
+                                "end": 5,
+                                "value": 2,
+                                "raw": "2"
+                            },
+                            "operator": "*",
+                            "right": {
+                                "type": "Literal",
+                                "start": 8,
+                                "end": 9,
+                                "value": 3,
+                                "raw": "3"
+                            }
+                        }
+                    },
+                    "operator": "+",
+                    "right": {
+                        "type": "Literal",
+                        "start": 12,
+                        "end": 13,
+                        "value": 4,
+                        "raw": "4"
+                    }
+                }
             }
         ],
         "sourceType": "module"
